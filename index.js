9dc682724ebccff8d5536a2431e05a50952da1e2
Adam Finch <atyreefinch@gmail.com>
2019-10-28 12:04:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+
+document.addEventListener("DOMContentLoaded", function() {
+textChanger();
+  });
+
+
+ 
+
+const textChanger = () => {
+    document.getElementById("text").innerHTML = "This is really cool!";
+}
